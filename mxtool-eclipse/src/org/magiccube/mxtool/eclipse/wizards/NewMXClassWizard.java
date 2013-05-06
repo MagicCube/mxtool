cf67f808a2a22f1f205ac8c7063262222c4916c8
Henry Li <henry.li03@sap.com>
2013-05-06 12:38:20 +0800

    a

--- mxtool-eclipse/src/org/magiccube/mxtool/eclipse/wizards/NewMXClassWizard.java
+++ mxtool-eclipse/src/org/magiccube/mxtool/eclipse/wizards/NewMXClassWizard.java
@@ -5,18 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
